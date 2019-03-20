8157bf631279e2d9459ddba3a201a81b3d28769d
Arrow <893091568@qq.com>
2019-03-20 14:05:18 +0800

    init

--- src/Http/Models/Crontab.php
+++ src/Http/Models/Crontab.php
@@ -3,7 +3,6 @@
 namespace Encore\Crontab\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Cron\CronExpression;
 
 class Crontab extends Model
 {
@@ -12,52 +11,5 @@ class Crontab extends Model
 
     public function crontabLog(){
         return $this->hasMany(CrontabLog::class, 'cid', 'id');
-    }
-    // 获取下次执行时间
-    public function getNextTimeAttr($value, $data){
-        $cron = CronExpression::factory($data['schedule']);
-        return $cron->getNextRunDate()->format('Y-m-d H:i');
-    }
-
-    public function getMaximumsTextAttr($value, $data){
-        if ($data['maximums']>0){
-            return $data['maximums'];
-        }else{
-            return '无限制';
-        }
-    }
-
-    public function setBeginTimeAttr($value)
-    {
-        return strtotime($value);
-    }
-
-    public function setEndTimeAttr($value)
-    {
-        return strtotime($value);
-    }
-
-    public function getBeginTimeAttr($value)
-    {
-        if (empty($value)){
-            return null;
-        }
-        return date('Y-m-d H:i', $value);
-    }
-
-    public function getEndTimeAttr($value)
-    {
-        if (empty($value)){
-            return null;
-        }
-        return date('Y-m-d H:i', $value);
-    }
-
-    public function getExecuteTimeAttr($value)
-    {
-        if (empty($value)){
-            return null;
-        }
-        return date('Y-m-d H:i', $value);
     }
 }
--- src/Http/Models/CrontabLog.php
+++ src/Http/Models/CrontabLog.php
@@ -13,33 +13,4 @@ class CrontabLog extends Model
     public function crontab(){
         return $this->belongsTo(Crontab::class, 'cid', 'id');
     }
-
-    public function getCidLinkAttr($value, $data)
-    {
-        $url = url('crontab/index/index', ['search_field'=>'id', 'keyword'=>$data['cid']]);
-        return '<a href="'.$url.'">'.$data['cid'].'</a>';
-    }
-
-    public function getTitleLinkAttr($value, $data)
-    {
-        $url = url('crontab/index/index', ['search_field'=>'id', 'keyword'=>$data['cid']]);
-        return '<a href="'.$url.'">'.$data['title'].'</a>';
-    }
-
-    public function getStatusTextAttr($value, $data)
-    {
-        if ($data['status']==1){
-            return '成功';
-        }else{
-            return '失败';
-        }
-    }
-
-    public function getExecuteTimeAttr($value, $data)
-    {
-        if (empty($data['create_time'])){
-            return null;
-        }
-        return date('Y-m-d H:i:s', $data['create_time']);
-    }
 }
